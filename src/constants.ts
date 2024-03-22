ef0fda9ea6a4cede849a1d25e2742c090e7811fb
YKDByJK <Jasonk@glaa.net>
2024-03-22 00:55:08 -0400

    token image

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,7 @@ export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? "https://api.ma
 export const PLATFORM_CREATOR_ADDRESS = new PublicKey('HvX1mNoVH7EaqB6KDdgzsHageuNBvbgrpPvnNcEpcTva')
 
 // Appears in ShareModal
-export const PLATFORM_SHARABLE_URL = 'v2-play.gamba.so'
+export const PLATFORM_SHARABLE_URL = 'v2-play.YKD.so'
 
 // List of tokens supported by this platform
 export const TOKENS: TokenMeta[] = [
@@ -18,7 +18,7 @@ export const TOKENS: TokenMeta[] = [
      mint: new PublicKey("BgFhv7WXJameaDeuwW5aSSGYyi6MpxE8yfyC2jYEssyy"),
     symbol: 'YKD',
     name: 'Yonder Kid Dreams',
-    image: "https://bafkreicezjydn2w7byv574zlftwptyi6fm6rklftpvnytmg2jwp3vkjhwq.ipfs.nftstorage.link",
+    image: <a href= "https://bafkreicezjydn2w7byv574zlftwptyi6fm6rklftpvnytmg2jwp3vkjhwq.ipfs.nftstorage.link"</a>,
      decimals: 6,
      baseWager: 1 * 1e6,
    }
