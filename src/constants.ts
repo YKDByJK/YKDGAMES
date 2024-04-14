ffc179155b51274e935b261bdea0d63451ece34e
YKDByJK <Jasonk@glaa.net>
2024-04-14 18:07:41 -0400

    please

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { GambaStandardTokens, TokenMeta } from 'gamba-react-ui-v2'
 import { PublicKey } from '@solana/web3.js'
 
 // Can be configured in .env
-export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? "https://go.getblock.io/c13490692bf64346bd7f71cef8b70dc2"
+export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? "https://api.mainnet-beta.solana.com"
 
 // Change this value to your Solana address
 export const PLATFORM_CREATOR_ADDRESS = new PublicKey('GUPCESUiXye7kh6kV9voGCk1C4LYukyuTwxLg7foYNRv')
